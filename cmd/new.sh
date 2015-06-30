a86d07323dd89a39a5665af1652053a219adc129
Zac YU <git@yuz.me>
2015-06-30 16:40:33 +0200

    add new.sh: create new wiki and start.sh: start a given wiki

---
+++ cmd/new.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+NPM_ROOT=$(npm run env | awk -F"=" '$1 ~ /PWD/ {print $2}')
+CONFIG_DIR="/Users/yu/Documents/Git/Configurations/TiddlyWiki"
+
+# 1. Copy config files from Repo
+
+if [ -z $1 ]; then
+    echo "Please provide me a folder name!"
+    return 1
+fi
+
+if [ -f ${CONFIG_DIR}/tiddlywiki.info ]; then
+    mkdir ${NPM_ROOT}/$1
+    cp ${CONFIG_DIR}/tiddlywiki.info ${NPM_ROOT}/$1/tiddlywiki.info
+    echo "Wiki creation done!"
+else
+    echo "Cannot create new wiki, Please check your configuration files at $CONFIG_DIR"
+    return 1
+fi
---
+++ cmd/start.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+NPM_ROOT=$(npm run env | awk -F"=" '$1 ~ /PWD/ {print $2}')
+BIN_DIR="${NPM_ROOT}/node_modules/.bin/"
+
+# 1. Check if tiddlywiki is installed
+
+if [ ! -d ${NPM_ROOT}/node_modules ]; then
+    echo "Running npm install"
+    npm install
+else
+    if [ ${BIN_DIR} != $(echo $PATH | awk -F":" '{ print $1 }') ]; then
+        PATH=${NPM_ROOT}/node_modules/.bin/:$PATH
+    fi
+fi
+
+# 2. Start tiddlywiki by given folder name
+
+if [ -z "$1" ]; then
+    echo "Please provide the name of the wiki (folder)"
+    return 1
+fi
+
+if [ -d "${NPM_ROOT}/$1" ]; then
+    tiddlywiki $1 --server &
+    open http://127.0.0.1:8080/
+else
+    echo "Wiki name not found!"
+    return 1
+fi
